3e20ffdd7153d129299b76704585922f362d3adc
AdarshMitr <adarshmitr@gmail.com>
2023-04-27 18:43:12 +0530

    adding context

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,9 +3,21 @@ import { useState, useEffect } from "react";
 import "../App";
 import { API_URL } from "../API";
 import axios from "axios";
+import { useAppContext } from "./context/appContext";
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
+
+
+  const {favorites,addToFavorites,removeFromFavorites}=useAppContext();
+
+
+console.log('favorites are:', favorites);
+
+const favoritesChecker=(id)=>{
+  const boolean=favorites.some((book)=>book.id===id);
+  return boolean;
+}
 
   useEffect(() => {
     axios
@@ -27,8 +39,11 @@ const BookList = () => {
           </div>
           <div><img src={book.image_url} alt='#'/> </div>
           <div>
-          
-            <button>Add to Favorites</button>
+          {favoritesChecker(book.id)?
+           <button onClick={()=>removeFromFavorites(book.id)}> Remove from Favorites</button>
+           :  <button onClick={()=>addToFavorites(book)}>Add to Favorites</button>
+          }
+            <button onClick={()=>addToFavorites(book)}>Add to Favorites</button>
           </div>
         </div>
       ))}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./components/context/appContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </BrowserRouter>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
