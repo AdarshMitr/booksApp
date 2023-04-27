7506c8d0f8d486a385a5aa755daa30fae46110e4
AdarshMitr <adarshmitr@gmail.com>
2023-04-27 13:31:51 +0530

    adding books API

---
+++ src/API.js
@@ -0,0 +1,1 @@
+export const API_URL='https://example-data.draftbit.com/books?_limit=50';
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import '../App';
+import React from "react";
+import { useState, useEffect } from "react";
+import "../App";
+import { API_URL } from "../API";
+import axios from "axios";
 
 const BookList = () => {
-  return (
-    <div className='book-list'>BookList</div>
-  )
-}
+  const [books, setBooks] = useState([]);
 
-export default BookList
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((res) => {
+        console.log(res.data);
+        setBooks(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="book-list">
+      {books.map((book) => (
+        <div className="book" key={book.id}>
+          <div>
+            
+            <h2>{book.title}</h2>
+          </div>
+          <div><img src={book.image_url} alt='#'/> </div>
+          <div>
+          
+            <button>Add to Favorites</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
