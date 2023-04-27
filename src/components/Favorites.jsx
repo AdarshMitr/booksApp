bc808af048b36ca93037384177af932a21a6c6ec
AdarshMitr <adarshmitr@gmail.com>
2023-04-27 19:00:10 +0530

    adding favorite page

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 import '../App';
+import { useAppContext } from './context/appContext';
+
+
+
 const Favorites = () => {
+
+  const {favorites,addToFavorites,removeFromFavorites}=useAppContext();
+
+
+  console.log('favorites are:', favorites);
+  
+  const favoritesChecker=(id)=>{
+    const boolean=favorites.some((book)=>book.id===id);
+    return boolean;
+  }
+
   return (
-    <div>Favorites</div>
+    <div className='favorites'>
+
+      {favorites.length>0 ?favorites.map((book) => (
+        <div className="book" key={book.id}>
+          <div>
+            
+            <h4>{book.title}</h4>
+          </div>
+          <div><img src={book.image_url} alt='#'/> </div>
+          <div>
+          {favoritesChecker(book.id)?
+           <button onClick={()=>removeFromFavorites(book.id)}> Remove from Favorites</button>
+           :  <button onClick={()=>addToFavorites(book)}>Add to Favorites</button>
+          }
+            
+          </div>
+        </div>
+      )): <h1>You don't have any favorite books yet!</h1> }
+      
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import '../App';
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
   return (
@@ -8,7 +10,7 @@ const Navbar = () => {
         <h1>Books App</h1>
       </div>
       <div>
-       Your Favorites
+      <Link to="/favorites"><h3>Your Favorites</h3></Link>
       </div>
     </div>
   );
